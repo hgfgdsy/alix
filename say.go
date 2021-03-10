5254f51a1cf1d42f49b9ef22d22d570829251627
hgfgdsy <1031010120@qq.com>
2021-03-10 15:46:42 +0800

    main-selfv2

--- say.go
+++ say.go
@@ -1,7 +1,11 @@
 package alix
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/hgfgdsy/alix/v2/sub"
+)
 
 func Nice(){
-	fmt.Println("33333333333333!")
+	fmt.Println("22222222222222!")
+	sub.Hoho()
 }
